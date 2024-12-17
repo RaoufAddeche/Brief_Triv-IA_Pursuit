4ea0df50d4b8c3f14789b705bea9c6eeef5176f9
NicolasCassonnet <nicolas.cassonnet@wanadoo.fr>
2024-12-17 16:36:56 +0100

    espoir

--- database_utils.py
+++ database_utils.py
@@ -75,6 +75,34 @@ class DatabaseUtils() :
                 game_id = id_game)
         
             session.add(new_player)
+            session.commit() 
+
+
+    def get_players(self, id_game : int) -> list[Player]:
+        list_players = []
+        with sm.Session(self.engine) as session:
+            statement = sm.select(Player).where(Player.game_id==id_game)
+            results = session.exec(statement)
+            list_players = list(results)
+        return list_players
+
+    def update_player(self, player :Player) :
+        with sm.Session(self.engine) as session:
+            statement = sm.select(Player).where(Player.id_player == player.id_player)
+            results = session.exec(statement)
+            session_player = results.one()
+            session_player.name = player.name
+            session_player.num_of_questions_with_bad_answer = player.num_of_questions_with_bad_answer
+            session_player.num_of_questions_with_correct_answer = player.num_of_questions_with_correct_answer
+            session_player.camembert_BASES_DE_DONNEES = player.camembert_BASES_DE_DONNEES
+            session_player.camembert_LANGAGES_DE_PROGRAMMATION = player.camembert_LANGAGES_DE_PROGRAMMATION
+            session_player.camembert_LIGNE_DE_COMMANDES = player.camembert_LIGNE_DE_COMMANDES
+            session_player.camembert_ACTUALITES_IA = player.camembert_ACTUALITES_IA
+            session_player.camembert_DEVOPS = player.camembert_DEVOPS
+            session_player.camembert_TECH_IA = player.camembert_TECH_IA
+            session_player.game_id = player.game_id 
+        
+            session.add(session_player)
             session.commit()
 
     #__________________________________________________________________________
